8da4bf991e14cf3a0258aa76206a0772db3afefd
seanlangbrown <seanlangbrown@gmail.com>
2018-02-02 17:07:09 -0800

    Added title and description from video in player

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,15 @@
 angular.module('video-player')
 
   .component('app', {
+
+    controller: function appController() {
+      console.log(window);
+      this.videos = window.exampleVideoData;
+      this.video = this.videos[0];
+      debugger;
+      console.log(this.videos[0]);
+      console.log(this.video);
+    },
+
     templateUrl: 'src/templates/app.html'
   });
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,12 @@
 angular.module('video-player')
 
   .component('videoPlayer', {
+    bindings: {
+      video: '<'
+    },
+
+    controller: function() { 
+    },
+
     templateUrl: 'src/templates/videoPlayer.html'
   });
